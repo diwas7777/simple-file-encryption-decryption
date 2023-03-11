3b980b4f3f1d0958fc9215d72e560d5bf0bbca52
diwas7777 <sthadiwash62@gmail.com>
2023-03-11 22:06:51 +0545

    initial commit

---
+++ decrypt.py
@@ -0,0 +1,24 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+    if file == "encrypt.py" or file == "key.key" or file == "decrypt.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+# print (files, "\n\n")
+
+with open("key.key", "rb") as thekey:
+    secretKey = thekey.read()
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_decrypted = Fernet(secretKey).decrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_decrypted)
+
+print(f"{files} hase been decrypted successfully")
